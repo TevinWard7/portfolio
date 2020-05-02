9e467b47155c7829752e2327281af3961b0f57cb
Tevin <tevinward7@gmail.com>
2020-05-02 17:29:22 -0400

    made hello display in random colors

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+// Hide pause icon before page loads
 $("#pause").hide();
 
 $(document).ready(function () {
 
+    // Display "Hello" in a random language & color
     const displayNewHello = () => {
 
+        // Array containing hello
         const helloArray = ["Hello", "こんにちは", "হ্যালো", "Привет", "ਸਤ ਸ੍ਰੀ ਅਕਾਲ"];
 
+        // Array containing colors 
+        const colors = [
+            "#ff0000", "#00ff00", "#ffcad4",
+            "#ff3333", "#ffff00"
+        ];
+
+        // Generates randomnumber
         const randomNumber = Math.floor(Math.random() * 5);
 
         $("#name-display").text(helloArray[randomNumber]);
+        $("#name-display").css('color', colors[randomNumber]);
 
     };
 
+    // Run the previous function every second
     const interval = setInterval(displayNewHello, 1000);
 
     const music = $("#music");
