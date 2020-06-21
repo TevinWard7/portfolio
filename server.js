246061bcf8d2711d49079003e3ea9904728f7c8d
Tevin <tevinward7@gmail.com>
2020-06-20 22:54:36 -0400

    made resume link work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require("express-handlebars");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
